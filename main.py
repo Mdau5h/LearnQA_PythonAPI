7918352adb0cbd536b7048b35055780d938d90f1
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-04-27 11:54:05 +0300

    Init commit

--- main.py
+++ main.py
@@ -1,14 +1,1 @@
-print("hello, world!")
-
-
-print("hello, world!")
-print("hello, world!")
-print("hello, world!")
-print("hello, world!")
-print("hello, world!")
-print("hello, world!")
-print("hello, world!")
-print("hello, world!")
-print("hello, world!")
-print("hello, world!")
-print("hello, world!")
+print('Hello from Ivan!')
