3d1a25cdc4a6078aee252a45d496daed19126e50
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-04-28 09:28:17 +0300

    valid method in loop

--- requests_and_methods.py
+++ requests_and_methods.py
@@ -6,7 +6,17 @@ methods = {
     'compare': 'compare_query_type'
 }
 
-compare = requests.request("HEAD", url + methods['compare'])
-print(compare.text)
-print(compare.status_code)
+request_types = [
+    'GET',
+    'POST',
+    'PUT',
+    'DELETE',
+]
 
+for type in request_types:
+    if type == 'GET':
+        compare = requests.request(type, url + methods['compare'], params={'method': type})
+    else:
+        compare = requests.request(type, url + methods['compare'], data={'method': type})
+    print(type)
+    print(compare)
