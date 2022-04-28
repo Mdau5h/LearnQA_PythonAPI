3c3c45048fd4c16ec412de04fe30d2a4de829381
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-04-28 09:14:44 +0300

    Update requests_and_methods.py

--- requests_and_methods.py
+++ requests_and_methods.py
@@ -6,16 +6,5 @@ methods = {
     'compare': 'compare_query_type'
 }
 
-request_types = [
-    'GET',
-    'POST',
-    'PUT',
-    'DELETE',
-    'HEAD'
-]
-
 compare = requests.request("GET", url + methods['compare'])
 print(compare.text)
-
-
-
