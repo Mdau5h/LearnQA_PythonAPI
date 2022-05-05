72de7de6d0dcc0550abcd3803749efaed1c1e0cd
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-05-05 16:18:19 +0300

    Update test_phrase.py

--- tests/test_phrase.py
+++ tests/test_phrase.py
@@ -1,5 +1,5 @@
 class TestPhrase():
     def test_phrase_check(self):
         phrase = input("Set a phrase: ")
-        assert len(phrase) < 15, "Phrase has more tahn 15 symbols in it"
+        assert len(phrase) < 15, "Phrase has more then 15 symbols in it"
         
