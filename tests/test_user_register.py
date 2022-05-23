5a4af227264c8a9bbe3e43c9d5663b7b7d74c478
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-05-23 08:48:29 +0300

    homework 1

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,3 +1,5 @@
+import pytest
+
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 from lib.my_requests import MyRequests
@@ -5,6 +7,12 @@ from lib.my_requests import MyRequests
 
 class TestUserRegister(BaseCase):
 
+    exclude_params = {
+        'password': '123',
+        'username': 'learnqa',
+        'firstName': 'learnqa',
+        'lastName': 'learnqa'
+    }
 
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
@@ -13,7 +21,6 @@ class TestUserRegister(BaseCase):
         Assertions.assert_json_has_key(response, 'id')
 
     def test_create_user_with_existing_email(self):
-
         email = 'vinkotov@example.com'
 
         data = self.prepare_registration_data(email)
@@ -21,3 +28,41 @@ class TestUserRegister(BaseCase):
         response = MyRequests.post("/user/", data=data)
         Assertions.assert_code_status(response, 400)
         assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
+
+    def test_create_user_with_incorrect_email(self):
+
+        email = 'vinkotovexample.com'
+
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == 'Invalid email format', f"Unexpected response content {response.content}"
+
+    @pytest.mark.parametrize('field_to_delete', exclude_params)
+    def test_create_user_without_one_field(self, field_to_delete):
+
+        email = 'vinkotov@example.com'
+        data = self.prepare_registration_data(email)
+        data.pop(field_to_delete)
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == f'The following required params are missed: {field_to_delete}', f"User created without required field: '{field_to_delete}'"
+
+    def test_create_user_with_short_firstname(self):
+        email = 'vinkotov@example.com'
+
+        data = self.prepare_registration_data(email)
+        data["firstName"] = 'a'
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode('utf-8') == f"The value of 'firstName' field is too short", f"User created with 'firstName' being too short"
+
+    def test_create_user_with_long_firstname(self):
+        email = 'vinkotov@example.com'
+        data = self.prepare_registration_data(email)
+        data["firstName"] = 'a'*251
+        response = MyRequests.post("/user/", data=data)
+        Assertions.assert_code_status(response, 400)
+        # print(response.content)
+        assert response.content.decode('utf-8') == f"The value of 'firstName' field is too long", f"User created with 'firstName' being too long"
