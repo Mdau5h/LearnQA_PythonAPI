1bf549e2bd52bae259268286aa7d984921e97dcb
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-05-23 09:04:14 +0300

    homework 4.1 tweaked

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -14,12 +14,14 @@ class TestUserRegister(BaseCase):
         'lastName': 'learnqa'
     }
 
+    # Успешное создание пользователя
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
         response = MyRequests.post("/user/", data=data)
         Assertions.assert_code_status(response, 200)
         Assertions.assert_json_has_key(response, 'id')
 
+    # Создание пользователя с уже существующим емейлом
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
 
@@ -29,6 +31,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode('utf-8') == f"Users with email '{email}' already exists", f"Unexpected response content {response.content}"
 
+    # Создание пользователя с емейлом без символа @
     def test_create_user_with_incorrect_email(self):
 
         email = 'vinkotovexample.com'
@@ -39,6 +42,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode('utf-8') == 'Invalid email format', f"Unexpected response content {response.content}"
 
+    # Создание пользователя без одного необходимого поля
     @pytest.mark.parametrize('field_to_delete', exclude_params)
     def test_create_user_without_one_field(self, field_to_delete):
 
@@ -49,6 +53,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode('utf-8') == f'The following required params are missed: {field_to_delete}', f"User created without required field: '{field_to_delete}'"
 
+    # Создание пользователя с очень коротким именем
     def test_create_user_with_short_firstname(self):
         email = 'vinkotov@example.com'
 
@@ -58,11 +63,11 @@ class TestUserRegister(BaseCase):
         Assertions.assert_code_status(response, 400)
         assert response.content.decode('utf-8') == f"The value of 'firstName' field is too short", f"User created with 'firstName' being too short"
 
+    # Создание пользователя с очень длинным именем
     def test_create_user_with_long_firstname(self):
         email = 'vinkotov@example.com'
         data = self.prepare_registration_data(email)
         data["firstName"] = 'a'*251
         response = MyRequests.post("/user/", data=data)
         Assertions.assert_code_status(response, 400)
-        # print(response.content)
         assert response.content.decode('utf-8') == f"The value of 'firstName' field is too long", f"User created with 'firstName' being too long"
