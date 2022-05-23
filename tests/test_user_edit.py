4a6bd4b75f17735ef59daee3fcc08d2272906f53
Ivan.V.Eliseev <AdesWepore73@gmail.com>
2022-05-23 12:12:00 +0300

    homework 4.3 (Ex17)

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -3,7 +3,17 @@ from lib.assertions import Assertions
 from lib.my_requests import MyRequests
 
 
-class TestUserEdut(BaseCase):
+class TestUserEdit(BaseCase):
+
+    def setup(self):
+        # Создаем тестового пользователя
+        self.test_data = self.prepare_registration_data()
+        print(self.test_data)
+        self.create_response = MyRequests.post("/user/", data=self.test_data)
+        Assertions.assert_code_status(self.create_response, 200)
+        Assertions.assert_json_has_key(self.create_response, 'id')
+
+    # Изменение данных только что созданного пользователя
     def test_edit_just_created_user(self):
 
         # Register
@@ -52,3 +62,86 @@ class TestUserEdut(BaseCase):
             new_name,
             "Wrong name of the user after edit"
         )
+
+    # Изменение данных пользователя, будучи неавторизованным
+    def test_edit_user_not_auth(self):
+        # edit
+        user_id = self.get_json_value(self.create_response, "id")
+        new_name = "Changed Name"
+        edit_response = MyRequests.put(f"/user/{user_id}", data={"firstName": new_name})
+        Assertions.assert_code_status(edit_response, 400)
+        print(edit_response.content)
+        assert edit_response.content.decode(
+            'utf-8') == f"Auth token not supplied", f"User edited without authorisation. Response content: '{edit_response.content}'"
+
+    # Изменение данных пользователя, будучи авторизованным другим пользователем
+    def test_edit_user_auth_by_wrong_user(self):
+        # login
+        login_data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        login_response = MyRequests.post("/user/login", data=login_data)
+        token = self.get_header(login_response, 'x-csrf-token')
+        auth_sid = self.get_cookie(login_response, "auth_sid")
+
+        # edit
+        user_id = self.get_json_value(self.create_response, "id")
+        print(user_id)
+        new_name = "Changed Name"
+
+        edit_response = MyRequests.put(f"/user/{user_id}",
+                                       data={"firstName": new_name},
+                                       headers={'x-csrf-token': token},
+                                       cookies={'auth_sid': auth_sid},
+                                       )
+        Assertions.assert_code_status(edit_response, 400)
+        assert edit_response.content.decode(
+            'utf-8') == 'Please, do not edit test users with ID 1, 2, 3, 4 or 5.',\
+            f"User edited with wrong token. Response content: '{edit_response.content}'"
+
+    # Изменение email пользователя, будучи авторизованным тем же пользователем, на новый email без символа @
+    def test_edit_user_incorrect_email(self):
+        # login
+        login_response = MyRequests.post("/user/login", data=self.test_data)
+        token = self.get_header(login_response, 'x-csrf-token')
+        auth_sid = self.get_cookie(login_response, "auth_sid")
+
+        # edit
+        user_id = self.get_json_value(self.create_response, "id")
+        print(user_id)
+        new_email = 'vinkotovexample.com'
+
+        edit_response = MyRequests.put(f"/user/{user_id}",
+                                       data={"email": new_email},
+                                       headers={'x-csrf-token': token},
+                                       cookies={'auth_sid': auth_sid},
+                                       )
+        Assertions.assert_code_status(edit_response, 400)
+        # print(edit_response.content)
+        assert edit_response.content.decode(
+            'utf-8') == 'Invalid email format', \
+            f"User edited with incorrect email. Response content: '{edit_response.content}'"
+
+    # Изменение firstName пользователя, будучи авторизованным тем же пользователем, на очень короткое значение в один символ
+    def test_edit_user_short_firstname(self):
+        # login
+        login_response = MyRequests.post("/user/login", data=self.test_data)
+        token = self.get_header(login_response, 'x-csrf-token')
+        auth_sid = self.get_cookie(login_response, "auth_sid")
+
+        # edit
+        user_id = self.get_json_value(self.create_response, "id")
+        print(user_id)
+        new_name = 'a'
+
+        edit_response = MyRequests.put(f"/user/{user_id}",
+                                       data={"firstName": new_name},
+                                       headers={'x-csrf-token': token},
+                                       cookies={'auth_sid': auth_sid},
+                                       )
+        Assertions.assert_code_status(edit_response, 400)
+        # print(edit_response.content)
+        assert edit_response.content.decode(
+            'utf-8') == '{"error":"Too short value for field firstName"}', \
+            f"User edited with short first name. Response content: '{edit_response.content}'"
--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -7,24 +7,6 @@ from lib.my_requests import MyRequests
 
 class TestUserGet(BaseCase):
 
-    ids = [
-        "0",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "10",
-        "11",
-        "12",
-        "13",
-        "14"
-    ]
-
     def test_get_user_details_not_auth(self):
         fields_response_should_not_have = [
             "email",
@@ -63,6 +45,7 @@ class TestUserGet(BaseCase):
         Assertions.assert_json_has_keys(check_response, expected_fields)
 
     def test_get_user_details_auth_as_another_user(self):
+        # auth
         data = {
             'email': 'vinkotov@example.com',
             'password': '1234'
@@ -77,12 +60,12 @@ class TestUserGet(BaseCase):
         auth_sid = self.get_cookie(auth_response, 'auth_sid')
         token = self.get_header(auth_response, "x-csrf-token")
         assert user_id_to_check != self.get_json_value(auth_response, "user_id"), "You authorised as the same user you trying to check"
-
+        # get user
         check_response = MyRequests.get(f"/user/{user_id_to_check}",
                                         headers={'x-csrf-token': token},
                                         cookies={'auth_sid': auth_sid}
                                         )
-
-        # print(check_response.content)
         Assertions.assert_json_has_key(check_response, "username")
         Assertions.assert_json_has_no_keys(check_response, fields_response_should_not_have)
+
+
